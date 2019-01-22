76518bcaab8a2e6d3fbe6776d3e18e197c2834f6
gichul kim <gichulkim@gichului-MacBook-Pro.local>
2019-01-22 15:51:36 +0900

    added

--- CodingCamp/Day7/P13305.java
+++ CodingCamp/Day7/P13305.java
@@ -4,11 +4,8 @@ import java.util.Scanner;
 public class P13305{
   public static void main(String[] args){
     Scanner sc=new Scanner(System.in);
-    int i,j;
-    int result=0;
-    int temp=0;
+    int i;
     int N=sc.nextInt();
-    int K=N;
     int[] edge=new int[N-1];
     int[] node=new int[N];
 
@@ -19,36 +16,12 @@ public class P13305{
        node[i]=sc.nextInt();
     }
 
-    for(;;){
-      int Y=0;
-      int min=1000000000;
-      //가장 작은 값
-      for(i=0; i<N; i++){
-        //같을수도 있는데 같은거는 > 함으로써 알아서 짤 min
-        if(node[i]!=0){
-          if(min>node[i]){
-            min=node[i];
-            temp=i;
-          }
-          if(node[i]==0)break;
-        }
-      }
-        node[temp]=0;
-      //같은 index의 edge(temp)부터 ~ 끝까지 더한 값을 나타내는 반복문 Y
-      for(i=temp; i<N-1; i++){
-        Y+=edge[i];
-        if(edge[i]==0)break;
-        edge[i]=0;
 
-      }
-      result+=min*Y;
-
-      if(node[temp]==node[0]&&node[0]==0)break;
-    }
+    
 
 
     System.out.println(result);
-    //
+
     // for(i=0; i<N-1; i++){
     //   System.out.print(edge[i]+" ");
     // }
