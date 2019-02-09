67d2e0d8fd6e12f135355032bc59df78713b5d30
gichul <kgc5551@naver.com>
2019-02-09 14:20:40 +0900

    added

---
+++ Algorithms/P2442.java
@@ -0,0 +1,21 @@
+import java.util.*;
+
+public class P2442{
+	public static void main(String[] args){
+		Scanner sc=new Scanner(System.in);
+		int N;
+		N=sc.nextInt();
+		for(int i=0; i<N; i++){
+			for(int j=N-1-i; j>0; j--){
+				System.out.print(" ");
+			}
+			for(int k=1; k<=2*i+1;k++){
+				System.out.print("*");
+			}
+		
+		System.out.println(" ");
+		}
+	}	
+
+
+}
