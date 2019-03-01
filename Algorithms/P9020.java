9b4041948059b62d3c0cf6a39bbd79be6d53db95
gichul <kgc5551@naver.com>
2019-03-01 17:00:59 +0900

    added

--- Algorithms/P9020.java
+++ Algorithms/P9020.java
@@ -1,17 +1,44 @@
 import java.util.*;
 
 public class P9020{
-	static int[] P=n
 	public static void main(String[] args){
 		Scanner sc=new Scanner(System.in);
 		int T;
 		T=sc.nextInt();
-		int i=0
-
-	
+		int i=0;
+		int[][] ans=new int[T][2];
 		while(i<T){
-			int x;
-			x=sc.nextInt();
+			int temp;
+			temp=sc.nextInt();
+			int x,y;
+			x=temp/2;
+			y=temp/2;
+			while(true){
+//	System.out.println(x+" " +y);
+//	break;
+				x=make_prime1(x);
+				y=make_prime2(y);
+				if(x+y==temp)break;
+				
+				int xx=x;
+				x--;
+	
+				x=make_prime1(x);
+				y=make_prime2(y);
+				if(x+y==temp)break;
+					
+				x=xx;
+				y++;				
+
+				x=make_prime1(x);
+				y=make_prime2(y);
+				if(x+y==temp)break;
+
+				x--;
+				y++;
+			}
+			ans[i][0]=x;
+			ans[i][1]=y;
 			
 
 	
@@ -19,8 +46,43 @@ public class P9020{
 	
 			i++;
 		}
+		for(i=0; i<T; i++){
+			System.out.println(ans[i][0]+" "+ans[i][1]);
+		}
+
+	}
 
 
+	static int make_prime1(int x){
+		boolean check=true;
+		while(check){
+			int count=0;
+			for(int i=2; i<x; i++){
+				if(x%i==0)count++;
+			}
+			if(count==0){
+				check=false;
+				return x;
+			}			
+			x--;
+		}	
+		return 0;
+	}
+
+	static int make_prime2(int y){
+		boolean check=true;
+		while(check){
+			int count=0;
+			for(int i=2; i<y; i++){
+				if(y%i==0)count++;
+			}
+			if(count==0){
+				check=false;
+				return y;
+			}			
+			y++;
+		}	
+		return 0;
 	}
 
 }
