a534a3b8a91f0ba27e2b2ced2dbbef596b24fc17
gichul <kgc5551@naver.com>
2019-03-01 19:44:35 +0900

    failed

--- Algorithms/P9020.java
+++ Algorithms/P9020.java
@@ -3,47 +3,47 @@ import java.util.*;
 public class P9020{
 	public static void main(String[] args){
 		Scanner sc=new Scanner(System.in);
+		int[] PS=new int[10001];	
 		int T;
+		find(PS);
+		int xx;
+//	for(int k=0;k<10001;k++){
+//		System.out.println(PS[k]);
+//	}
+
+
 		T=sc.nextInt();
 		int i=0;
+		int x,y;
+		int temp;
 		int[][] ans=new int[T][2];
-		while(i<T){
-			int temp;
+		while(i<T){	
 			temp=sc.nextInt();
-			int x,y;
 			x=temp/2;
 			y=temp/2;
 			while(true){
+				x=make_prime1(x,PS);
+				y=make_prime2(y,PS);
 //	System.out.println(x+" " +y);
-//	break;
-				x=make_prime1(x);
-				y=make_prime2(y);
 				if(x+y==temp)break;
-				
-				int xx=x;
+				xx=x;
 				x--;
-	
-				x=make_prime1(x);
-				y=make_prime2(y);
+				x=make_prime1(x,PS);
 				if(x+y==temp)break;
-					
 				x=xx;
-				y++;				
-
-				x=make_prime1(x);
-				y=make_prime2(y);
-				if(x+y==temp)break;
-
-				x--;
 				y++;
+				y=make_prime1(y,PS);
+				if(x+y==temp)break;
+				x--; y++;
+			
 			}
 			ans[i][0]=x;
 			ans[i][1]=y;
-			
 
-	
 
-	
+
+
+
 			i++;
 		}
 		for(i=0; i<T; i++){
@@ -53,36 +53,47 @@ public class P9020{
 	}
 
 
-	static int make_prime1(int x){
+	static int make_prime1(int x,int[] PS){
+		int i=0;
 		boolean check=true;
 		while(check){
-			int count=0;
-			for(int i=2; i<x; i++){
-				if(x%i==0)count++;
-			}
-			if(count==0){
+			if(PS[x-i]==1){
 				check=false;
-				return x;
-			}			
-			x--;
-		}	
+				return x-i;
+			}	
+			i++;
+		}
 		return 0;
 	}
 
-	static int make_prime2(int y){
+	static int make_prime2(int y,int[] PS){
+		int i=0;
 		boolean check=true;
 		while(check){
-			int count=0;
-			for(int i=2; i<y; i++){
-				if(y%i==0)count++;
-			}
-			if(count==0){
+			if(PS[y+i]==1){
 				check=false;
-				return y;
-			}			
-			y++;
-		}	
+				return y+i;
+			}	
+			i++;
+		}
 		return 0;
 	}
+
+	static void find(int[] PS){
+		int count;
+		for(int i=2; i<10001; i++){
+			count=0;
+			for(int j=2; j<i; j++){
+				if(i%j==0)count++;
+			}	
+			if(count==0){
+				PS[i]=1;
+			}
+		}
+
+		return;
+	}
+
+
 
 }
