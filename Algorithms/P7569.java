6f61b5bcfcf2542873b83ecc3b9f89e1e34e5e23
gichul <kgc5551@naver.com>
2019-03-12 23:29:32 +0900

    added

--- Algorithms/P7569.java
+++ Algorithms/P7569.java
@@ -1,79 +1,104 @@
 import java.util.*;
-import java.io.*;
-
-class Graph{
-	private int V;
-	private LinkedList<Integer> adj[];
-
-	Graph(int v){
-		V=v;
-		adj=new LinkedList[v];
-		for(int i=0; i<v; i++){
-			adj[i]=new LinkedList();
-		}
-	}
-
-	void addEdge(int v, int w){adj[v].add(w);}
-
-	void BFS(ins s){
-		boolean visite[]=new boolean[V];
-		LinkedList<Integer> queue=new LinkedList<Integer?();
-		
-		visited[s]=true;
-		queue.add(s);
-		
-		while(queue.size()!=0){
-			s=queue.poll();
-		System.out.print(s+ " ");
-	
-		Iterator<Integer> i=adj[s].listIterator();
-			while(i.hasNext()){
-				int n=i.next();
-				if(!visiter[n]){
-					visited[n]=true;
-					queue.add(n);
-				}
-			}	
-		}
-	
-	}
-}
-
-
-
-
 
 public class P7569{
 	static int[][][] B;
+	static int N,M,H;
+	static int ans=0;
+	static Queue<Integer> q=new LinkedList<>();	
+	
 	public static void main(String[] args){
 		Scanner sc=new Scanner(System.in);
-		int N,M,H;
 		N=sc.nextInt();
 		M=sc.nextInt();
 		H=sc.nextInt();
 		B=new int[H][M][N];	
-		
+		int count=0;
+		int num=0;
+		int temp=0;
 		for(int i=0; i<H; i++){
 			for(int j=0; j<M; j++){
 				for(int k=0; k<N; k++){
 					B[i][j][k]=sc.nextInt();
+					if(B[i][j][k]==0)count++;
+					if(B[i][j][k]==1&&temp==0){
+						num++;q.add(i);q.add(j);q.add(k);
+					}	
 				}
 			}
+		}
+		if(count==0){
+			System.out.println(0);
+			return ;
 		}	
-			
+		int x=0,y=0,z=0;
+		count=0;
+		while(!q.isEmpty()){
+			ans++;
+			count=0;
+			for(int m=0; m<num; m++){
+				x=q.remove();
+				y=q.remove();
+				z=q.remove();
+				count+=change(x,y,z);	
+				B[x][y][z]=2;		
+			}
+			num=count;
+		}
+
+                for(int i=0; i<H; i++){
+                        for(int j=0; j<M; j++){
+                                for(int k=0; k<N; k++){
+                                	if(B[i][j][k]==0){
+						System.out.println(-1);	
+						return ;
+					}
+				}
+                        }
+                }
 		
 
+		System.out.println(ans-1);
 		
-
-
 
 	
 	}
+	
 		
-
-
-		void ripe(int num){
-			
-			
+	static int change(int i,int j,int k){
+		int count=0;
+		if(k+1<N&&B[i][j][k+1]==0){
+			B[i][j][k+1]=1;
+			q.add(i);q.add(j);q.add(k+1);
+			count++;
+		}		
+		if(k-1>=0&&B[i][j][k-1]==0){
+			B[i][j][k-1]=1;
+			q.add(i);q.add(j);q.add(k-1);
+			count++;
+		}		
+		if(j+1<M&&B[i][j+1][k]==0){
+			B[i][j+1][k]=1;	
+			q.add(i);q.add(j+1);q.add(k);
+			count++;
+		}	
+		if(j-1>=0&&B[i][j-1][k]==0){
+			B[i][j-1][k]=1;
+			q.add(i);q.add(j-1);q.add(k);
+			count++;
+		}		
+		if(i+1<H&&B[i+1][j][k]==0){
+			B[i+1][j][k]=1;
+			q.add(i+1);q.add(j);q.add(k);
+			count++;
+		}		
+		if(i-1>=0&&B[i-1][j][k]==0){
+			B[i-1][j][k]=1;
+			q.add(i-1);q.add(j);q.add(k);
+			count++;
 		}
+		return count; 		
+	}
+
+			
+			
 }
