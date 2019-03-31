c1b66033ff3957b6aa225cb041203860200d6b8d
gichul <kgc5551@naver.com>
2019-04-01 00:04:10 +0900

    added

--- MyDS/Sort/BubbleSort.java
+++ MyDS/Sort/BubbleSort.java
@@ -1,7 +1,7 @@
 import java.util.*;
 
 
-public class BubbleSort{
+public class ubbleSort{
         static int[] S=new int[]{5,2,3,4,1,6,7,0,11,9};
 
         public static void main(String[] args){
