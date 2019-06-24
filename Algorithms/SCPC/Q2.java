cd8abed68268db8df55f98679aa8a4017fc762b1
gichul <kgc5551@naver.com>
2019-06-24 16:24:31 +0900

    added

---
+++ Algorithms/SCPC/Q2.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+class Q2{
+	static int Answer;
+
+	public static void main(String args[]) throws Exception	{
+		Scanner sc = new Scanner(System.in);
+
+
+
+
+
+
+		
+		
+		
+		
+		/*
+		int T = sc.nextInt();
+		for(int test_case = 0; test_case < T; test_case++) {
+			// Answer = 0;
+
+			System.out.println("Case #"+(test_case+1));
+			System.out.println(Answer);
+		}*/
+	}
+}
