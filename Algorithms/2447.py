33a595380e6eac3e6118161ea7cc81ba0bd7ae9a
gitcheol <kgc5551@naver.com>
2020-03-20 21:29:45 +0900

    added

--- Algorithms/2447.py
+++ Algorithms/2447.py
@@ -1,3 +1,16 @@
 N=int(input())
 
 
+def star(i,j):
+    while i!=0:
+        if (i%3 == 1 and j%3 == 1) : 
+            print(' ',end='')
+            return None
+        i=i//3
+        j=j//3
+    print("*",end='')
+
+for i in range(N):
+    for j in range(N):
+        star(i,j)
+    print("")
